acccc2d894a24356e497cd11064fe308c8f9ebd6
shuihai <99359473@qq.com>
2017-11-22 16:03:35 +0800

    4

--- j5/src/test/java/com/tj/PeopleTest.java
+++ j5/src/test/java/com/tj/PeopleTest.java
@@ -2,8 +2,11 @@ package com.tj;
 
 import junit.framework.TestCase;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class PeopleTest extends TestCase {
@@ -29,15 +32,48 @@ public class PeopleTest extends TestCase {
 
     public void testSetName() throws Exception {
         Driver driver =new  com.mysql.jdbc.Driver();
-        String url="jdbc:mysql://localhost:3306/test";
+        String url="jdbc:mysql://127.0.0.1:3306/test";
         Properties info=new Properties();
         info.put("user","root");
-        info.put("password","");
+        info.put("password","123456");
         Connection connection=driver.connect(url,info);
         System.out.println(connection);
     }
 
-    public void testTest1(){
-        System.out.println("test11");
+    public void testProperty(){
+        Properties properties=new Properties();
+        properties.put("user","root");
+        System.out.println(properties);
     }
+
+    public void testClassforname() throws ClassNotFoundException {
+        System.out.println(Class.forName("java.lang.String"));
+    }
+
+    public void testgetclassgetclassloadergetresourceasstream() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        InputStream in=getClass().getClassLoader().getResourceAsStream("jdbc.properties");
+        Properties properties=new Properties();
+        properties.load(in);
+
+        String driverclass = properties.getProperty("driver");
+        String url = properties.getProperty("jdbcurl");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+
+        Driver driver= (Driver) Class.forName(driverclass).newInstance();
+        Properties info =new Properties();
+        info.put("user","root");
+        info.put("password","123456");
+        Connection connection=driver.connect(url,info);
+        System.out.println(connection);
+
+    }
+
+
+    public void getConnection() throws Exception{
+        String driverclass="mysql";
+        Driver driver = (Driver) Class.forName(driverclass).newInstance();
+
+    }
+
 }
