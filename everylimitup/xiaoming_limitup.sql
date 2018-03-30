073c8fcd6cd166336f25955eb0c15d84a8dbe0c5
shuihai <99359473@qq.com>
2018-03-30 18:30:49 +0800

    e

---
+++ everylimitup/xiaoming_limitup.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2018-03-30 12:30:14
+-- 服务器版本： 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `xiaoming_limitup`
+--
+
+CREATE TABLE IF NOT EXISTS `xiaoming_limitup` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `code` varchar(12) NOT NULL,
+  `stockname` varchar(64) NOT NULL,
+  `reason` int(11) NOT NULL,
+  `first_time` int(11) NOT NULL,
+  `last_time` int(11) NOT NULL,
+  `consistent_day` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
